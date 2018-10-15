2a75177163836b395810c0786c4b857dc459fa8b
SrikantP <spandey570@gmail.com>
2018-10-15 12:03:22 +0530

    Created Copy of Base & Tc1 file

--- src/test/java/Copy/Basic.java
+++ src/test/java/Copy/Basic.java
@@ -1,4 +1,4 @@
-package demo.maven;
+package Copy;
 
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -6,25 +6,26 @@ import org.testng.annotations.Parameters;
 
 public class Basic {
 
-	
+	@Parameters({ "Environment" })
 	@BeforeMethod
-	public void EnvironmentSetup() {
+	public void EnvironmentSetup( String Environment) {
 
-		if (System.getProperty("env1").equalsIgnoreCase("prod")) {
-			System.out.println("Abhishek prod env");
-		}
-		
-		if (System.getProperty("env1").equalsIgnoreCase("QA")) {
-			System.out.println("Srii QA env");
-		}
+//		if (System.getProperty("env1").equalsIgnoreCase("prod")) {
+//			System.out.println("Abhishek prod env");
+//		}
+//		
+//		if (System.getProperty("env1").equalsIgnoreCase("QA")) {
+//			System.out.println("Srii QA env");
+//		}
 
-		/*if (Environment.equalsIgnoreCase("Prod")) {
+		if (Environment.equalsIgnoreCase("Prod")) {
 			String UrlP = "https://www.flipkart.com/";
 			System.out.println("Production Environment");
-		} else if (Environment.equalsIgnoreCase("QA")) {
+		}
+		else if (Environment.equalsIgnoreCase("QA")) {
 			String UrlQA = "https://www.amazon.in/?tag=googinhydmabk-21&ref_=pd_mn_ABKror78";
 			System.out.println("QA Environment");
-		}*/
+		}
 
 	}
 
