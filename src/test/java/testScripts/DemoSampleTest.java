48cd0ae868c77b991c45218b0cc37240d6565f27
Srikant Pandey <srikant.pandey@tothenew.com>
2020-04-27 12:44:32 +0530

    Added demo test file in testng xml

--- src/test/java/testScripts/DemoSampleTest.java
+++ src/test/java/testScripts/DemoSampleTest.java
@@ -4,15 +4,24 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
-
+import org.testng.annotations.Test;
 
 public class DemoSampleTest {
 
-    public static void main(String[] args)
+    @Test
+    public void verifyInvalidLogin()
     {
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+    //  WebDriverManager for setting up execution on chrome browser
+
+    //  WebDriverManager.chromedriver().setup();
+    //  WebDriver driver = new ChromeDriver();
+
+    //  WebDriverManager for setting up execution on firefox browser
+        WebDriverManager.firefoxdriver().setup();
+        WebDriver driver = new FirefoxDriver();
+
         driver.get("https://www.saucedemo.com/");
         driver.manage().window().maximize();
         driver.findElement(By.id("user-name")).sendKeys("admin");
@@ -21,7 +30,5 @@ public class DemoSampleTest {
         String errorMsg= driver.findElement(By.xpath("//*[@data-test='error']")).getText();
         Assert.assertEquals(errorMsg,"Epic sadface: Username and password do not match any user in this service");
         driver.quit();
-
-
     }
 }
